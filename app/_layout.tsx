c0f72dcf1ad3496d2c4516262f2ab52d77f9eaa4
francosegura <franco.ssegura@hotmail.com>
2023-07-25 14:53:45 -0300

    Con error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,6 +43,7 @@ function RootLayoutNav() {
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(randomStack)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
       </ThemeProvider>
